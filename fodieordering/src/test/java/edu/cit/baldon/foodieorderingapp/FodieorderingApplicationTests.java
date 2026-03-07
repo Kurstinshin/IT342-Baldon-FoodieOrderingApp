24ab75a2c5f4bc79a057deca475517d9b7687096
unknown <kirstenshaneb@gmail.com>
2026-03-07 23:40:36 +0800

    Resolve merge conflicts, keep local axios 1.5.0

---
+++ fodieordering/src/test/java/edu/cit/baldon/foodieorderingapp/FodieorderingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.baldon.foodieorderingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FodieorderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
