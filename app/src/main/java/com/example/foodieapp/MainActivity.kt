be5bb5df5e14b8736fa4d0687ee6bffcc00266b6
LAB13_USER ON WS19 <L13Y12W19@LabHCI.net>
2026-03-28 13:34:37 +0800

    Foodie App with login/register/API

---
+++ app/src/main/java/com/example/foodieapp/MainActivity.kt
@@ -0,0 +1,21 @@
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.foodieapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnRegister).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
